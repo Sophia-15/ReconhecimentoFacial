79efadd0e01f2fd2c88e19e74a46ad3c57d1487a
sophia <deletrasopa@gmail.com>
2024-07-02 19:24:23 -0300

    adding access possibility

--- src/main.py
+++ src/main.py
@@ -12,14 +12,16 @@ while True:
   face_locations, face_names = sfr.detect_known_faces(frame)
 
   for face_loc, name in zip(face_locations, face_names):
+    access_text = "Acesso Permitido" if people[name] else "Acesso Negado"
     y1, x2, y2 , x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-    y2 += 32
+    y1 -= 24
+    y2 += 48
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
 
     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
 
-    cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 0, 255), cv2.FILLED)
-    font = cv2.FONT_HERSHEY_DUPLEX
-    cv2.putText(frame, name, (x1 + 6, y2 - 6), font, 1, (255, 255, 255), 1)
+    cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 0, 200), cv2.FILLED)
+    cv2.putText(frame, access_text, (x1 + 6, y2 - 6), font, 1, (255, 255, 255), 1)
 
   cv2.imshow("Camera", frame)
 
